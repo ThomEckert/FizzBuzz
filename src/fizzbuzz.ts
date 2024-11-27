6646321bed760c629fb3911626b2f435b8dd9e51
ThomEckert <thec.lernt@gmx.de>
2024-11-27 19:14:19 +0100

    initiate fizzbuzz.ts and function fizzBuzz

---
+++ src/fizzbuzz.ts
@@ -0,0 +1,18 @@
+export function fizzBuzz(n: number): string[] {
+    const result: string[] = [];
+    if (n <= 0) {
+        throw new Error("n must be greater than 0");
+    }
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i.toString());
+        }
+    }
+    return result;
+}
